0546c24dd98b2a224c482552d2073af61cd29d1f
LJDunford <98477912+LJDunford@users.noreply.github.com>
2023-06-30 14:52:08 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,11 +11,11 @@ else
 	sleep 1
 fi
 
-if docker image history eu.gcr.io/lbg-mea-12/python-app:ldunford-v${old_version} > /dev/null; then
-	docker rmi eu.gcr.io/lbg-mea-12/python-app:ldunford-v${old_version}
+if docker image history eu.gcr.io/lbg-mea-12/lbg-python-ldunford:ldunford-v${old_version} > /dev/null; then
+	docker rmi eu.gcr.io/lbg-mea-12/lbg-python-ldunford:ldunford-v${old_version}
 else
 	sleep 1
 fi
 
-docker build -t eu.gcr.io/lbg-mea-12/python-app:ldunford-v${version} .
-docker run -d -p 80:${PORT} -e PORT=${PORT} --name lbg-sample eu.gcr.io/lbg-mea-12/python-app:ldunford-v${version}
+docker build -t eu.gcr.io/lbg-mea-12/lbg-python-ldunford:ldunford-v${version} .
+docker run -d -p 80:${PORT} -e PORT=${PORT} --name lbg-sample eu.gcr.io/lbg-mea-12/lbg-python-ldunford:ldunford-v${version}
